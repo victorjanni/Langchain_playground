1d7301efe3f031c8231135fe438f84822c41c34b
Victor Janni <54091482+victorjanni@users.noreply.github.com>
2026-06-12 15:03:16 +0530

    basic llm demo with openai added

---
+++ Basics_of_Langchain/1.LLMs/1_llm_demo.py
@@ -0,0 +1,11 @@
+
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model='gpt-5-mini')
+response = llm.invoke("what is rag?")
+
+print(response.content)
+
