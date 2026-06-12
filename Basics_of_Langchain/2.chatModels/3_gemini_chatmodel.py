715bd262af9c99c0b2a12761901fcb9fb43c3615
Victor Janni <54091482+victorjanni@users.noreply.github.com>
2026-06-12 15:25:47 +0530

    gemini chatmodel demo added

---
+++ Basics_of_Langchain/2.chatModels/3_gemini_chatmodel.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+response = llm.invoke("what is lanchain?")
+
+print(response.content)
